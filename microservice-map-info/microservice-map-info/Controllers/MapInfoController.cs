68d034622b81e9306d3dca224c944b0e81fc86b8
AliNoorkazemi <alinoorkazemi@gmail.com>
2022-06-24 19:47:51 +0430

    Add project files.

---
+++ microservice-map-info/microservice-map-info/Controllers/MapInfoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("[controller]")]
+[Route("[controller]/[action]")]
+[ApiController]
+public class MapInfoController : ControllerBase
+{
+    public readonly GoogleDistanceApi _googleDistanceApi;
+    public MapInfoController(GoogleDistanceApi googleDistanceApi)
+    {
+        _googleDistanceApi = googleDistanceApi;
+    }
+
+    [HttpGet]
+    public async Task<GoogleDistanceData> GetDistance(string originCity, string destinationCity)
+    {
+        return await _googleDistanceApi.GetMapDistance(originCity, destinationCity);
+    }
+}
